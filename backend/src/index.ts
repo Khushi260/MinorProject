388bc66b873173834123200b0e178df42580543c
Khushi260 <khushi261002@gmail.com>
2023-10-15 12:06:56 +0530

    connected db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,19 @@ import express from "express";
 
 const app = express();
 
+// GET - 
+// PUT
+// POST 
+// DELETE
+
+
+//middleware
+app.use(express.json())
+
+app.post("/")
+
+app.get("/hello",(req,res,next)=>{
+   return res.send("Hello");
+});
+
 app.listen(5000,()=>console.log("Server Open"));
