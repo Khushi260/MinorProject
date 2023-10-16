aa408c9326ef25d85cc15ee975252c78f232aa9f
Khushi260 <khushi261002@gmail.com>
2023-10-16 12:03:47 +0530

    added pages and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-
-import './App.css'
+import Header from "./components/Header"
+import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Chat from "./pages/Chat"
+import Notfound from "./pages/Notfound"
 
 function App() {
- 
 
-  return <main>Hello World</main>
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
+      </Routes>
+
+    </main>
+  )
+
 }
 
 export default App
